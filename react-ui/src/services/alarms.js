811dee3eecf0cc6d11f0f2a920b54a7cef6ed369
Alejandro Galue <agalue@opennms.org>
2017-08-10 12:14:40 -0400

    Simplifying the alarms-service.

--- react-ui/src/services/alarms.js
+++ react-ui/src/services/alarms.js
@@ -14,8 +14,4 @@ export default class AlarmsService {
     return fetch(`${URL}?limit=0`, REQUEST).then(response => response.json());
   }
 
-  static getAlarm(alarmId) {
-    return fetch(`${URL}/${alarmId}`, REQUEST).then(response => response.json());
-  }
-
 }

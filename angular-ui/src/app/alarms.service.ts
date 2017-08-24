c1fde1deef05e5eff4cd94fa3f252f780ed7e3d5
Alejandro Galue <agalue@opennms.org>
2017-08-24 14:03:43 -0400

    Upgrading Angular and fixing HttpHeaders usage

--- angular-ui/src/app/alarms.service.ts
+++ angular-ui/src/app/alarms.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
 import { Alarm } from './alarm';
@@ -12,18 +13,26 @@ export class AlarmsService {
   constructor(private http: HttpClient) { }
 
   private getOptions() : Object {
-    let headers = new HttpHeaders();
-    headers.set('Authorization', 'Basic ' + btoa('demo:demo'));
-    headers.set('Accept', 'application/json');
-    return { headers: headers, withCredentials: true };    
+    let headers = new HttpHeaders().set('Authorization', 'Basic ' + btoa('demo:demo'));
+    return { headers };
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    const msg = err.error instanceof Error ? err.error.message : err.statusText;
+    return Observable.throw(msg);
   }
 
   getAlarms() : Promise<Alarm[]> {
-    return this.http.get<Alarm[]>(`${this.BASE_URL}?limit=0`, this.getOptions()).map(data => data['alarm'] as Alarm[]).toPromise();
+    return this.http.get<Alarm[]>(`${this.BASE_URL}?limit=0`, this.getOptions())
+      .catch(this.handleError)
+      .map(data => data['alarm'] as Alarm[])
+      .toPromise();
   }
 
   getAlarm(id: number) : Promise<Alarm> {
-    return this.http.get<Alarm>(`${this.BASE_URL}/${id}`, this.getOptions()).toPromise();
+    return this.http.get<Alarm>(`${this.BASE_URL}/${id}`, this.getOptions())
+      .catch(this.handleError)
+      .toPromise();
   }
 
 }
